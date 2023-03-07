556b76cbb9920a02c921414d87d45a95da0bd6f0
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-03-07 10:19:25 +0700

    add screpperjs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+app.get('/tracking', (req, res) => {
+    
 })
 app.listen(process.env.PORT || 3000)
