b54b442130ae1977dbbb3fbfe123b114eed36448
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-03-07 10:25:34 +0700

    add phantomjs

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const cheerio = require('cheerio');
 const axios = require('axios');
+const scraperjs = require('scraperjs');
 
 app.get('/tracking', (req, res) => {
-    
+    scraperjs.DynamicScraper.create('https://spx.co.id/m/tracking-detail/SPXID030360806343')
+    .scrape(function($) {
+        return $(".log-item").map(function() {
+            return $(this).find(".log-item-status").text();
+        }).get();
+    })
+    .then(function(news) {
+        console.log(news);
+    })
 })
 app.listen(process.env.PORT || 3000)
