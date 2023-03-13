8477374f974bedcdaf1cb7deb09776b3e030a790
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-03-13 17:47:41 +0700

    Update API KEY

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 app.get('/tracking/:resi', async (req, res) => {
     const resi = req.params.resi
-    const url = 'https://everpro.id/fitur-cek-resi/php/lacak_paket.php';
+    const url = `https://api.binderbyte.com/v1/track?api_key=cb978c0a4a9574dafc1e630885296ea231bb7964cea07ebf34e638672f66a347&courier=spx&awb=${resi}`;
     var data = new FormData();
     data.append('resi', resi);
     data.append('kurir', 'spx');
