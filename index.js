754e3ca7c9988f9bf5b954fee48cde102d38ff66
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-03-13 17:51:16 +0700

    Update API KEY

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const axios = require('axios');
 app.get('/tracking/:resi', async (req, res) => {
     const resi = req.params.resi
     const url = `https://api.binderbyte.com/v1/track?api_key=cb978c0a4a9574dafc1e630885296ea231bb7964cea07ebf34e638672f66a347&courier=spx&awb=${resi}`;
-    var data = new FormData();
-    data.append('resi', resi);
-    data.append('kurir', 'spx');
-    axios.post(url, data)
-        .then(response => {
-            res.send(response.data)
-        })
+    const response = await axios.get(url);
+    res.send(response.data);
 })
 app.listen(process.env.PORT || 3000)
